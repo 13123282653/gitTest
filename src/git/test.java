a96857b0ad3ac0ce8ef9f1d702732640977297dc
wangzhen <1729736693@qq.com>
2022-05-22 22:16:53 +0800

    push commit

--- src/git/test.java
+++ src/git/test.java
@@ -4,7 +4,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hot-fix");
-        System.out.println("push ");
+        System.out.println("推送到远程库");
 
     }
 }
